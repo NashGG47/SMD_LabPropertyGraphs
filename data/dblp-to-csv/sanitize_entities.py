11045b1f70fd8490993cfcae30e9cb4f513fd4bf
unknown <nashly.erielis.gonzalez@estudiantat.upc.edu>
2025-03-23 22:50:29 +0100

    Feat: Adding dblp-to-csv code and instructions.

---
+++ data/dblp-to-csv/sanitize_entities.py
@@ -0,0 +1,17 @@
+import re
+
+input_file = 'dblp.xml'
+output_file = 'dblp_clean.xml'
+
+# Expresión regular para entidades como &ouml;, &uuml;, etc.
+entity_pattern = re.compile(r'&[a-zA-Z]+;')
+
+with open(input_file, 'r', encoding='utf-8', errors='ignore') as fin, \
+     open(output_file, 'w', encoding='utf-8') as fout:
+    
+    for line in fin:
+        cleaned_line = entity_pattern.sub('', line)
+        fout.write(cleaned_line)
+
+print("✔️ Archivo limpio guardado como:", output_file)
+
