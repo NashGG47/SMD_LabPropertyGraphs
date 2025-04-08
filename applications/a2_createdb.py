4f90eddaee3e10ecf86e44840a794c54f37d17fa
CeciPePe <ceci.perez.perez@gmail.com>
2025-04-08 14:53:51 +0200

    fix a2 module

---
+++ applications/a2_createdb.py
@@ -0,0 +1,9 @@
+from neo4j_pipeline.import_data_toneo4j import load_all
+
+def runA2_createdb():
+    load_all()
+    print('Data has been loaded into Neo4j database.')
+
+if __name__ == '__main__':
+    runA2_createdb()
+    print('Pipeline A2 has been executed successfully.')
--- applications/a2_loaddata.py
+++ applications/a2_loaddata.py
@@ -1,18 +1,18 @@
 from semanthic_scholar.get_data import get_test_data, createSC_to_CSV
 from semanthic_scholar.publication_venues_processing import split_csv
-from semanthic_scholar.data_relations import generateRelations, generate_synthetic_keywords, extract_topics
-from neo4j_pipeline.import_data_toneo4j import load_all
+from semanthic_scholar.data_relations import generateRelations, generate_synthetic_keywords, extract_topics, edit_some_editions_volumes
 
-def runA2():
+def runA2_load_data():
     get_test_data()
     createSC_to_CSV()
     split_csv()
     generateRelations()
     generate_synthetic_keywords()
     extract_topics()
-    load_all()
+    edit_some_editions_volumes()
     print('Semantic scholar data has been processed and relations have been generated.')
 
+
 if __name__ == '__main__':
-    runA2()
+    runA2_load_data()
     print('Pipeline A2 has been executed successfully.')
